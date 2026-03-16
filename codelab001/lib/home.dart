76a21045a9f4e5363968456c613a7aec300a8909
escoto34 <laramanchas34@gmail.com>
2026-03-15 22:00:33 -0600

    modified:   codelab001/lib/home.dart 	new file:   codelab001/lib/models/product.dart 	new file:   codelab001/lib/models/products_repository.dart 	new file:   codelab001/lib/supplemental/asymmetric_view.dart 	new file:   codelab001/lib/supplemental/cut_corners_border.dart 	new file:   codelab001/lib/supplemental/product_card.dart 	new file:   codelab001/lib/supplemental/product_columns.dart 	modified:   codelab001/pubspec.lock 	modified:   codelab001/pubspec.yaml

--- codelab001/lib/home.dart
+++ codelab001/lib/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'models/product.dart';
+import 'models/products_repository.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +13,36 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   @override
+  // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(int count) {
+    List<Card> cards = List.generate(count, (int index) {
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18.0 / 11.0,
+              child: Image.asset('assets/diamond.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const <Widget>[
+                  Text('Title'),
+                  SizedBox(height: 8.0),
+                  Text('Secondary Text'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+    return cards;
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       // TODO: Add app bar (102)
@@ -36,36 +70,12 @@ class HomePageState extends State<HomePage> {
           ),
         ],
       ),
+      // TODO: Add a grid view (102)
       body: GridView.count(
         crossAxisCount: 2,
         padding: const EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
-        // TODO: Build a grid of cards (102)
-        children: <Widget>[
-          Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                AspectRatio(
-                  aspectRatio: 18.0 / 11.0,
-                  child: Image.asset('assets/diamond.png'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Title'),
-                      const SizedBox(height: 8.0),
-                      Text('Secondary Text'),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        children: _buildGridCards(10), // Replace
       ),
       resizeToAvoidBottomInset: false,
     );
