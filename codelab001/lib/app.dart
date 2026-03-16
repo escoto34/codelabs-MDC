f348956b00f2ef16c8d794332bbf9f04a70fe251
escoto34 <laramanchas34@gmail.com>
2026-03-15 22:07:20 -0600

    modified:   codelab001/lib/app.dart 	new file:   codelab001/lib/colors.dart

--- codelab001/lib/app.dart
+++ codelab001/lib/app.dart
@@ -1,6 +1,5 @@
-
 import 'package:flutter/material.dart';
-
+import 'colors.dart';
 import 'home.dart';
 import 'login.dart';
 
@@ -21,11 +20,26 @@ class ShrineApp extends StatelessWidget {
         // TODO: Pass _currentCategory for frontLayer (104)
         // TODO: Change backLayer field value to CategoryMenuPage (104)
       },
-      // TODO: Customize the theme (103)
-      theme: ThemeData.light(useMaterial3: true),
+      theme: _kShrineTheme, // New code
     );
   }
 }
 
-// TODO: Build a Shrine Theme (103)
+final ThemeData _kShrineTheme = _buildShrineTheme();
+
+ThemeData _buildShrineTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    colorScheme: base.colorScheme.copyWith(
+      primary: kShrinePink100,
+      onPrimary: kShrineBrown900,
+      secondary: kShrineBrown900,
+      error: kShrineErrorRed,
+    ),
+    // TODO: Add the text themes (103)
+    // TODO: Add the icon themes (103)
+    // TODO: Decorate the inputs (103)
+  );
+}
+
 // TODO: Build a Shrine Text Theme (103)
