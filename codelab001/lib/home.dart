b4a4a84a56f33eb00840e10b01a395d347e72bc1
escoto34 <laramanchas34@gmail.com>
2026-03-15 22:05:15 -0600

    modified:   codelab001/lib/home.dart

--- codelab001/lib/home.dart
+++ codelab001/lib/home.dart
@@ -13,65 +13,62 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   @override
-  // TODO: Make a collection of cards (102)
-  // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
 
-// Replace this entire method
-List<Card> _buildGridCards(BuildContext context) {
-  List<Product> products = ProductsRepository.loadProducts(Category.all);
+    if (products.isEmpty) {
+      return const <Card>[];
+    }
 
-  if (products.isEmpty) {
-    return const <Card>[];
-  }
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+      locale: Localizations.localeOf(context).toString(),
+    );
 
-  final ThemeData theme = Theme.of(context);
-  final NumberFormat formatter = NumberFormat.simpleCurrency(
-      locale: Localizations.localeOf(context).toString());
-
-  return products.map((product) {
-    return Card(
-      clipBehavior: Clip.antiAlias,
-      // TODO: Adjust card heights (103)
-      child: Column(
-        // TODO: Center items on the card (103)
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          AspectRatio(
-            aspectRatio: 18 / 11,
-            child: Image.asset(
-              product.assetName,
-              package: product.assetPackage,
-             // TODO: Adjust the box size (102)
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-              child: Column(
-               // TODO: Align labels to the bottom and center (103)
-               crossAxisAlignment: CrossAxisAlignment.start,
-                // TODO: Change innermost Column (103)
-                children: <Widget>[
-                 // TODO: Handle overflowing labels (103)
-                 Text(
-                    product.name,
-                    style: theme.textTheme.titleLarge,
-                    maxLines: 1,
-                  ),
-                  const SizedBox(height: 8.0),
-                  Text(
-                    formatter.format(product.price),
-                    style: theme.textTheme.titleSmall,
-                  ),
-                ],
+    return products.map((product) {
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        // TODO: Adjust card heights (103)
+        child: Column(
+          // TODO: Center items on the card (103)
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18 / 11,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                fit: BoxFit.fitWidth,
               ),
             ),
-          ),
-        ],
-      ),
-    );
-  }).toList();
-}
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  // TODO: Align labels to the bottom and center (103)
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // TODO: Change innermost Column (103)
+                  children: <Widget>[
+                    // TODO: Handle overflowing labels (103)
+                    Text(
+                      product.name,
+                      style: theme.textTheme.titleLarge,
+                      maxLines: 1,
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.titleSmall,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -102,12 +99,12 @@ List<Card> _buildGridCards(BuildContext context) {
       ),
       // TODO: Add a grid view (102)
       // TODO: Add a grid view (102)
-body: GridView.count(
-  crossAxisCount: 2,
-  padding: const EdgeInsets.all(16.0),
-  childAspectRatio: 8.0 / 9.0,
-  children: _buildGridCards(context) // Changed code
-),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        children: _buildGridCards(context), // Changed code
+      ),
       resizeToAvoidBottomInset: false,
     );
   }
