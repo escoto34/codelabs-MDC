c568d674508d84c7c2600904ac61dc0c38c0e995
escoto34 <laramanchas34@gmail.com>
2026-03-15 21:26:23 -0600

    modified:   codelab001/lib/home.dart

--- codelab001/lib/home.dart
+++ codelab001/lib/home.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
-import 'login.dart';
-
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,12 +10,20 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-        return Scaffold(
+    return Scaffold(
       // TODO: Add app bar (102)
-      // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.menu, semanticLabel: 'menu'),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+        title: const Text('SHRINE'),
+        // TODO: Add trailing buttons (102)
       ),
+      // TODO: Add a grid view (102)
+      body: Center(child: Text('You did it!')),
       resizeToAvoidBottomInset: false,
     );
   }
